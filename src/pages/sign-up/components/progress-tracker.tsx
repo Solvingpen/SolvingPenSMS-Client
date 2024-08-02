2cca659a74b9b60535f16386abfc863bf0c536e4
Onah Victor <victoronah.dev@gmail.com>
2024-08-02 13:46:36 +0100

    Added check icons to registration tracking ui

--- src/pages/sign-up/components/progress-tracker.tsx
+++ src/pages/sign-up/components/progress-tracker.tsx
@@ -1,16 +1,22 @@
+import { FaCheck } from "react-icons/fa6";
+
 /** UI for displaying form progress */
 const ProgressTracker = ({ currentPage }: { currentPage: 1 | 2 | 3 }) => {
 	return (
 		<div className="flex items-center">
-			<button className="h-6 w-6 rounded-full bg-[#228D67]"></button>
+			<button className="h-6 w-6 rounded-full bg-[#228D67] flex items-center justify-center">
+				{currentPage > 1 && <FaCheck className="text-white" />}
+			</button>
 			<span className="h-1 flex-1 bg-black"></span>
 			<button
-				className={`h-6 w-6 rounded-full ${
+				className={`h-6 w-6 flex items-center justify-center rounded-full ${
 					currentPage >= 2 ? "bg-[#228D67]" : "bg-[#D9D9D9]"
-				}`}></button>
+				}`}>
+				{currentPage > 2 && <FaCheck className="text-white" />}
+			</button>
 			<span className="h-1 flex-1 bg-black"></span>
 			<button
-				className={`h-6 w-6 rounded-full ${
+				className={`h-6 w-6 flex items-center justify-center rounded-full ${
 					currentPage > 2 ? "bg-[#228D67]" : "bg-[#D9D9D9]"
 				}`}></button>
 		</div>
