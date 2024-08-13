9587eeb9984297e14c43536ee80833718954a45a
Onah Victor <victoronah.dev@gmail.com>
2024-08-13 15:03:14 +0100

    Minor changes to reginald's branch

--- src/pages/forgot-password/components/ForgotPasswordFooter.tsx
+++ src/pages/forgot-password/components/ForgotPasswordFooter.tsx
@@ -1,16 +1,26 @@
 const ForgotPasswordFooter = () => {
-  return (
-    <>
-      <div className="flex flex-col justify-center items-center pt-20">
-        <p className="text-xs sm:text-sm lg:text-xl md:text-xl">
-          Contact Support: Schoolname@gmail.com
-        </p>
-        <p className="text-xs sm:text-sm lg:text-xl md:text-xl">
-          School Phone: 08065005323
-        </p>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="flex flex-col justify-center items-center pt-20">
+				<p className="text-sm">
+					Contact Support:{" "}
+					<a
+						className="text-blue-600 hover:underline"
+						href="mailto:Schoolname@gmail.com">
+						Schoolname@gmail.com
+					</a>
+				</p>
+				<p className="text-sm">
+					School Phone:{" "}
+					<a
+						className="text-blue-600 hover:underline"
+						href="tel:+2348065005323">
+						08065005323
+					</a>
+				</p>
+			</div>
+		</>
+	);
 };
 
 export default ForgotPasswordFooter;
--- src/pages/sign-in/components/SigninForm.tsx
+++ src/pages/sign-in/components/SigninForm.tsx
@@ -7,118 +7,119 @@ import { useNavigate } from "react-router-dom";
 import Checkbox from "../../../components/checkbox";
 
 interface Signin {
-  login: "";
-  username: "";
-  password: "";
+	login: "";
+	username: "";
+	password: "";
 }
 
 const SigninForm = () => {
-  const [formData, setFormData] = useState<Signin>({} as Signin);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm({ mode: "onBlur" });
-  const navigate = useNavigate();
+	const [formData, setFormData] = useState<Signin>({} as Signin);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid }
+	} = useForm({ mode: "onBlur" });
+	const navigate = useNavigate();
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value, type, checked } = e.target;
-    const inputvalue = type === 'checkbox' ? checked : value
-    setFormData((prev) => ({
-      ...prev,
-      [name]: inputvalue,
-    }));
-  };
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value, type, checked } = e.target;
+		const inputvalue = type === "checkbox" ? checked : value;
+		setFormData(prev => ({
+			...prev,
+			[name]: inputvalue
+		}));
+	};
 
-  const onSubmit = async () => {
-    try {
-      if (isValid) {
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        console.log(formData);
-        navigate("/");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="lg:w-[320px] md:w-[320px] w-100 m-auto"
-      >
-        <div className=" mb-3">
-          <Input
-            autoComplete=""
-            errors={errors}
-            register={register}
-            id="username"
-            name="username"
-            label="Username"
-            placeholder="Username"
-            onChange={handleChange}
-            type="text"
-            errorMessages={{
-              invalid: "Use a valid first name",
-              empty: "username cannot be empty",
-            }}
-            pattern={RegExp(/^[A-Za-zÀ-ÖØ-öø-ÿ'’-]+$/) as unknown as string}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <Input
-            autoComplete="new-password"
-            errors={errors}
-            register={register}
-            id="password"
-            name="password"
-            label="Password"
-            type="password"
-            onChange={handleChange}
-            errorMessages={{
-              invalid:
-                "No spacing in password. Password must be at least six characters long",
-              empty: "Password cannot be empty",
-            }}
-            pattern={RegExp(/^[^\s]{6,}$/) as unknown as string}
-            required
-          />
-        </div>
-        <div className="flex items-center gap-2 mb-3">
-          <Checkbox
-            id={"reminder"}
-            label={"Remind me of my password when am here"}
-            name={"reminder"}
-            register={register}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <p className="text-sm">
-            Forgot password? click{" "}
-            <Link
-              to={"/forgot-password"}
-              className="text-[#007BFF] hover:underline"
-            >
-              here
-            </Link>
-          </p>
-        </div>
-        <div className="flex justify-between mb-3">
-          <Link
-            to={"/"}
-            className="text-sm inline-flex bg-[#6C757D] px-6 py-2 rounded-[6px] disabled:cursor-not-allowed disabled:opacity-70 text-[#F7F9FA] "
-          >
-            BACK
-          </Link>
-          <button className="text-sm inline-flex bg-[#007BFF] px-6 py-2 rounded-[6px] disabled:cursor-not-allowed disabled:opacity-70 text-[#F7F9FA]">
-            LOGIN
-          </button>
-        </div>
-      </form>
-    </>
-  );
+	const onSubmit = async () => {
+		try {
+			if (isValid) {
+				await new Promise(resolve => setTimeout(resolve, 1000));
+				console.log(formData);
+				navigate("/");
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	return (
+		<>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className="lg:w-[320px] md:w-[320px] w-100 m-auto">
+				<div className=" mb-3">
+					<Input
+						autoComplete=""
+						errors={errors}
+						register={register}
+						id="username"
+						name="username"
+						label="Username"
+						placeholder="Username"
+						onChange={handleChange}
+						type="text"
+						errorMessages={{
+							invalid: "Use a valid first name",
+							empty: "username cannot be empty"
+						}}
+						pattern={
+							RegExp(
+								/^[A-Za-zÀ-ÖØ-öø-ÿ'’-]+$/
+							) as unknown as string
+						}
+						required
+					/>
+				</div>
+				<div className="mb-3">
+					<Input
+						autoComplete="new-password"
+						errors={errors}
+						register={register}
+						id="password"
+						name="password"
+						label="Password"
+						type="password"
+						onChange={handleChange}
+						errorMessages={{
+							invalid:
+								"No spacing in password. Password must be at least six characters long",
+							empty: "Password cannot be empty"
+						}}
+						pattern={RegExp(/^[^\s]{6,}$/) as unknown as string}
+						required
+					/>
+				</div>
+				<div className="flex items-center gap-2 mb-3">
+					<Checkbox
+						id={"reminder"}
+						label={"Remember me"}
+						name={"reminder"}
+						register={register}
+						onChange={handleChange}
+					/>
+				</div>
+				<div className="mb-3">
+					<p className="text-sm">
+						Forgot password? click{" "}
+						<Link
+							to={"/forgot-password"}
+							className="text-[#007BFF] hover:underline">
+							here
+						</Link>
+					</p>
+				</div>
+				<div className="flex justify-between mb-3">
+					<Link
+						to={"/"}
+						className="text-sm inline-flex bg-[#6C757D] px-6 py-2 rounded-[6px] disabled:cursor-not-allowed disabled:opacity-70 text-[#F7F9FA] ">
+						BACK
+					</Link>
+					<button className="text-sm inline-flex bg-[#007BFF] px-6 py-2 rounded-[6px] disabled:cursor-not-allowed disabled:opacity-70 text-[#F7F9FA]">
+						LOGIN
+					</button>
+				</div>
+			</form>
+		</>
+	);
 };
 
 export default SigninForm;
